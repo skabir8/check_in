ec3d1b45554438f4a18e5ce58c3fabdc28dd3e18
Shariar Kabir <shariar.k8@gmail.com>
2018-06-19 15:50:32 -0400

    added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,22 +3,29 @@ var router = express.Router();
 var fs = require('fs');
 
 
+var sqlite3 = require('sqlite3').verbose();
 
+function getName(callback){
+  var db = new sqlite3.Database('data/users.db');
+  var query = "SELECT * FROM events";
+  db.all(query, function (err, rows) {
+    if(err){
+        console.log(err);
+    }else{
+        callback(rows[0]);
+    }
+  });
+}
+
+
+function print_data(data){
+  console.log(data);
+}
+
+getName(print_data)
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // Comment out this line:
-  //res.send('respond with a resource');
 
-  // And insert something like this instead:
-  var users;
-  var exists = fs.existsSync('data.json');
-  if (exists) {
-    var txt = fs.readFileSync('data.json', 'utf8');
-    data = JSON.parse(txt);
-  } else {
-    data = {};
-  }
-  console.log(data);
   res.json(data);
 });
 
