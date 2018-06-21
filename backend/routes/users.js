81112cd452dbd6bea30f2659a7925b92983d771b
Shariar Kabir <shariar.k8@gmail.com>
2018-06-21 14:12:33 -0400

    added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -42,6 +42,7 @@ router.get('/', function(req, res, next) {
         res.send((data));
     }
   });
+  db.close();
   //console.log(getEventData(sendLocations, 0));
   //res.json(getName(print_data));
 });
